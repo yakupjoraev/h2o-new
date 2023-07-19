0699fb876d4a3b0ee8dc4eb2a39b5063b19b0269
Yakup <yakupjoraev@gmail.com>
2023-07-19 15:26:37 +0300

    added new modals

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -1,101 +1,17 @@
 // Custom Scripts
 // Custom scripts
 
-function filtersArchive() {
-  const container = document.querySelector('.archive')
 
-  if (!container) {
-    return null
-  }
-
-  const filtersResult = document.querySelector('[data-filters-result]');
-  const filtersBtn1 = document.querySelector('[data-filters-btn-1]');
-  const filtersBtn2 = document.querySelector('[data-filters-btn-2]');
-
-  filtersBtn1.addEventListener('click', () => {
-    filtersResult.classList.remove('view-list');
-    filtersResult.classList.add('view-cards');
-
-    filtersBtn2.classList.remove('view-list');
-    filtersBtn1.classList.add('view-cards');
-  })
-
-  filtersBtn2.addEventListener('click', () => {
-    filtersResult.classList.remove('view-cards');
-    filtersResult.classList.add('view-list');
-
-    filtersBtn1.classList.remove('view-cards');
-    filtersBtn2.classList.add('view-list');
-  })
-}
-
-filtersArchive();
 
 // селект для сводной таблицы
 
-$(function () {
-  $("#table-form-select").selectmenu();
-});
+// $(function () {
+//   $("#table-form-select").selectmenu();
+// });
 
 // подключение и настройка jq ui календаря
 
 $(function () {
-  $("#datepicker-1").datepicker({
-    changeMonth: false,
-    changeYear: true,
-    showOn: "button",
-    buttonImage: "./img/icons/datepicker.svg",
-    // buttonImageOnly: true,
-    buttonText: "Выбрать дату",
-  });
-
-  $("#datepicker-2").datepicker({
-    changeMonth: false,
-    changeYear: true,
-    showOn: "button",
-    buttonImage: "./img/icons/datepicker.svg",
-    // buttonImageOnly: true,
-    buttonText: "Выбрать дату",
-  });
-
-  $("#datepicker-3").datepicker({
-    changeMonth: false,
-    changeYear: true,
-    showOn: "button",
-    buttonImage: "./img/icons/datepicker.svg",
-    // buttonImageOnly: true,
-    buttonText: "Выбрать дату",
-  });
-
-  $("#datepicker-4").datepicker({
-    changeMonth: false,
-    changeYear: true,
-    showOn: "button",
-    buttonImage: "./img/icons/datepicker.svg",
-    // buttonImageOnly: true,
-    buttonText: "Выбрать дату",
-  });
-
-  $("#datepicker-5").datepicker({
-    changeMonth: false,
-    changeYear: true,
-    showOn: "button",
-    buttonImage: "./img/icons/datepicker.svg",
-    // buttonImageOnly: true,
-    buttonText: "Выбрать дату",
-  });
-
-  $("#datepicker-6").datepicker({
-    changeMonth: false,
-    changeYear: true,
-    showOn: "button",
-    buttonImage: "./img/icons/datepicker.svg",
-    // buttonImageOnly: true,
-    buttonText: "Выбрать дату",
-  });
-
-
-
 
   $("#datepicker-10").datepicker({
     changeMonth: false,
@@ -147,6 +63,72 @@ $(function () {
   });
 
 
+  $("#datepicker-15").datepicker({
+    changeMonth: false,
+    changeYear: true,
+    showOn: "button",
+    buttonImage: "./img/icons/datepicker.svg",
+    // buttonImageOnly: true,
+    buttonText: "Выбрать дату",
+  });
+
+
+
+  $("#datepicker-16").datepicker({
+    changeMonth: false,
+    changeYear: true,
+    showOn: "button",
+    buttonImage: "./img/icons/datepicker.svg",
+    // buttonImageOnly: true,
+    buttonText: "Выбрать дату",
+  });
+
+
+
+  $("#datepicker-17").datepicker({
+    changeMonth: false,
+    changeYear: true,
+    showOn: "button",
+    buttonImage: "./img/icons/datepicker.svg",
+    // buttonImageOnly: true,
+    buttonText: "Выбрать дату",
+  });
+
+
+
+  $("#datepicker-18").datepicker({
+    changeMonth: false,
+    changeYear: true,
+    showOn: "button",
+    buttonImage: "./img/icons/datepicker.svg",
+    // buttonImageOnly: true,
+    buttonText: "Выбрать дату",
+  });
+
+
+
+  $("#datepicker-19").datepicker({
+    changeMonth: false,
+    changeYear: true,
+    showOn: "button",
+    buttonImage: "./img/icons/datepicker.svg",
+    // buttonImageOnly: true,
+    buttonText: "Выбрать дату",
+  });
+
+
+
+  $("#datepicker-20").datepicker({
+    changeMonth: false,
+    changeYear: true,
+    showOn: "button",
+    buttonImage: "./img/icons/datepicker.svg",
+    // buttonImageOnly: true,
+    buttonText: "Выбрать дату",
+  });
+
+
+
   /* Локализация datepicker */
   $.datepicker.regional['ru'] = {
     closeText: 'Закрыть',
@@ -170,6 +152,62 @@ $(function () {
 
 
 
-// вызов модалки
+// // вызов модалки
 
-const modal = new GraphModal();
+// const modal = new GraphModal();
+
+const openModalBtns = document.querySelectorAll('.open-modal-btn');
+const closeModalBtns = document.querySelectorAll('.close-modal-btn');
+const modals = document.querySelectorAll('.modal');
+
+openModalBtns.forEach(btn => {
+  btn.addEventListener('click', () => {
+    const modalId = btn.dataset.modalId;
+    const modal = document.getElementById(modalId);
+    modal.classList.add('show');
+  });
+});
+
+closeModalBtns.forEach(btn => {
+  btn.addEventListener('click', () => {
+    const modal = btn.closest('.modal');
+    modal.classList.remove('show');
+  });
+});
+
+window.addEventListener('click', (event) => {
+  if (event.target.classList.contains('modal')) {
+    event.target.classList.remove('show');
+  }
+});
+
+
+function filtersArchive() {
+  const container = document.querySelector('.archive')
+
+  if (!container) {
+    return null
+  }
+
+  const filtersResult = document.querySelector('[data-filters-result]');
+  const filtersBtn1 = document.querySelector('[data-filters-btn-1]');
+  const filtersBtn2 = document.querySelector('[data-filters-btn-2]');
+
+  filtersBtn1.addEventListener('click', () => {
+    filtersResult.classList.remove('view-list');
+    filtersResult.classList.add('view-cards');
+
+    filtersBtn2.classList.remove('view-list');
+    filtersBtn1.classList.add('view-cards');
+  })
+
+  filtersBtn2.addEventListener('click', () => {
+    filtersResult.classList.remove('view-cards');
+    filtersResult.classList.add('view-list');
+
+    filtersBtn1.classList.remove('view-cards');
+    filtersBtn2.classList.add('view-list');
+  })
+}
+
+filtersArchive();
